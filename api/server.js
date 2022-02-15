cbdbc0a1be1b4cc699d1cc1c57a3c31c2bd2cb29
Akhil P <akhilpinnani96@gmail.com>
2022-02-15 11:34:46 -0800

    final push

--- api/server.js
+++ api/server.js
@@ -6,6 +6,8 @@ const server = express();
 
 server.use(express.json());
 
+server.use('/api/accounts', accountsRouter);
+
 server.use('*', (req, res) =>{
     res.status(404).json({
         message: 'not found',
